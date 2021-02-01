f35c3bfde09154ceb6d4fbb853641f0fec5e846a
nmorse <morse.nate@gmail.com>
2021-02-01 17:52:28 -0500

    vite konva and pounce all in one

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { KonvaCanvas } from './KonvaCanvas';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <KonvaCanvas pounceCode={
+          `
+          [uncons uncons pop [id shape xy w] [shape square == [xy [w 2 / +] map] [xy] if-else] pounce] [get-xy] compose
+          [stack-copy [get-xy] map
+          [concat] map2
+          [[l-1 stripe] swap concat] map
+          uncons uncons uncons 
+          #[] [uncons drop] reduce uncons
+          ] 
+           [make-stripes] compose 
+          make-stripes 
+          `}
+          shapes={
+            `
+            [c-1 circle 100 200 20] [s-1 square 200 100 40]
+            [c-2 circle 100 210 20] [s-2 square 210 100 40]
+            [c-3 circle 100 220 20] [s-3 square 220 100 40]
+            `
+          }
+        ></KonvaCanvas>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/KonvaCanvas.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import {
+  Stage,
+  Layer,
+  Rect,
+  Circle,
+} from 'react-konva';
+// import Konva from 'konva';
+
+// Rect, Text, Circle and line as just react compoents
+// they are not loaded into Konva namespace
+
+// so we need to import required shapes manually
+import "konva/lib/shapes/Rect";
+import "konva/lib/shapes/Circle";
+import { parse, unParse , interpreter} from '@pounce-lang/core';
+
+
+const paintElement = (adjust: any, coords: any, color: any = "tan") => {
+  // console.log(coords);
+  
+  switch (coords[1]) {
+    case 'square':
+      return <Rect
+        x={coords[2]}
+        y={coords[3]}
+        width={coords[4]}
+        height={coords[4]}
+        fill="pink"
+        shadowBlur={20}
+        draggable
+        onDragMove={(e: any) => {
+          const attr = e?.target?.attrs || {x: 0, y: 0};
+          const newCoords = [coords[0], coords[1], attr.x, attr.y, coords[4]] 
+          adjust(newCoords);
+        }}
+      //onDragEnd
+      />;
+      break;
+    case 'circle':
+      return <Circle
+        x={coords[2]}
+        y={coords[3]}
+        radius={coords[4]}
+        fill="pink"
+        shadowBlur={20}
+        draggable
+        onDragMove={(e: any) => {
+          const attr = e.target.attrs;
+          const newCoords = [coords[0], coords[1], attr.x, attr.y, coords[4]] 
+          adjust(newCoords);
+        }}
+      />;
+      break;
+    case 'stripe':
+      return <Rect
+        x={coords[2]}
+        y={coords[3]}
+        width={coords[4]-coords[2]}
+        height={coords[5]-coords[3]}
+        fill="red"
+        opacity={0.5}
+        draggable
+      />;
+      break;
+    default:
+  }
+};
+
+const pounceOn = (shapes: any, code: any) => {
+  const upShapes = unParse(shapes);
+  const cat = interpreter(upShapes + " " + code);
+  const canvasCmds = cat.next()?.value?.stack;
+  return canvasCmds;
+};
+
+
+export const KonvaCanvas = (props: any) => {
+  const [shapes, setPs] = useState(parse(props.shapes));
+  const [canvasCmds, setCc] = useState(pounceOn(shapes, props.pounceCode));
+  
+  const alterShape = (i: any) => (pSnippet: any) => {
+    let newPs = [...shapes]; 
+    newPs[i] = pSnippet;
+    setPs(newPs);
+    setCc(pounceOn(newPs, props.pounceCode))
+  };
+  
+  return (
+    <Stage style={{ width: 670, height: 500, backgroundColor: "#ffffff" }}
+      width={670} height={500}>
+      <Layer>
+        {
+          canvasCmds.filter((e: any) => e[1] === 'stripe')
+          .map((coordinate: any, i: any) => paintElement(alterShape(i), coordinate))
+        }
+        {
+          canvasCmds.filter((e: any) => e[1] !== 'stripe')
+          .map((coordinate: any, i: any) => paintElement(alterShape(i), coordinate))
+        }
+      </Layer>
+    </Stage>
+  );
+};
